6ab8cf15af2ce98834a910733a5979e72305bcac
liugang3138 <liugang@wingspan.cn>
2020-11-12 11:43:40 +0800

    jenkings

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Second msg="hello vue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Second from './components/Second'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Second
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const title = 'hello vue'
+
 module.exports = {
   publicPath: './',
   outputDir: 'dist',
-  filenameHashing: false
+  filenameHashing: false,
+  configureWebpack: {
+    name: title
+  }
 }
